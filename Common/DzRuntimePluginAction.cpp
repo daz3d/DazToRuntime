73d3f6db5aa08acda5968cb5fc4cc0f59bdf73b4
David-Vodhanel <39318287+David-Vodhanel@users.noreply.github.com>
2022-04-09 16:49:48 -0700

    Fix for animations not transferring

--- Common/DzRuntimePluginAction.cpp
+++ Common/DzRuntimePluginAction.cpp
@@ -302,6 +302,10 @@ void DzRuntimePluginAction::ExportNode(DzNode* Node)
 		  ExportOptions.setBoolValue("doLights", false);
 		  ExportOptions.setBoolValue("doCameras", false);
 		  ExportOptions.setBoolValue("doAnims", false);
+		  if (AssetType == "Animation")
+		  {
+			  ExportOptions.setBoolValue("doAnims", true);
+		  }
 		  if ((AssetType == "Animation" || AssetType == "SkeletalMesh") && ExportMorphs && MorphString != "")
 		  {
 				ExportOptions.setBoolValue("doMorphs", true);
